4d8851251794871bc2988e1b9244f0735870a34b
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-09-04 20:31:01 -0400

    declared vafiables for character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+console.log('ready')
+$('tweeterMessage').on('input', function()) {
+const messageLength = $(this).val().length; 
+const messageLimit = 140; //max 140 characters
+const spaceRemaining = messageLimit - messageLength;
+
+}
+});
---
+++ public/styles/composer-char-counter.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("DOM has loaded ")
+});
+
+https://flex-web.compass.lighthouselabs.ca/p/3/workbooks/web-flex-v2-m04w8/activities/611?journey_step=81&workbook=51
