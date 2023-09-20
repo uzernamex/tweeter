0a4f79806bb1c4b680b9b971b1bed1a9d7035b84
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-09-19 20:30:10 -0400

    Update client.js to render tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -22,17 +22,6 @@ const $tweet = createTweetElement(tweetData);
 console.log($tweet); // to see what it looks like
 $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
-{
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
 
 const data = [
   {
@@ -76,3 +65,28 @@ return $tweet;
 }
 
 renderTweets(data);
+
+
+
+// // Test / driver code (temporary). Eventually will get this from the server.
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+// const $tweet = createTweetElement(tweetData);
+
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+// const $tweet = $(`<article class="tweet">Hello world</article>`);
+
