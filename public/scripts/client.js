2202c1c16c4f34b05c51de7112c6c7ef92849dd0
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-09-30 14:06:59 -0400

    Refactor and lint for organization

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
 
 // S A M P L E   T W E E T S
 
@@ -52,33 +53,40 @@ const createTweetElement = function(tweet) {
 
   $tweet.append($userDetails);
   
-  // $tweet.append($("<p>").text(tweet.content.text));
+  const $tweetLine = $("<div>").addClass("tweet-line").append($("<p>").text(tweet.content.text));
   
-  const $tweetLine = $("<div>").addClass("tweet-line");
-  $tweetLine.append($("<p>").text(tweet.content.text));
-  $tweet.append($userDetails);
-  $tweet.append($tweetLine);
+  // $tweet.append($userDetails);
+  // $tweet.append($tweetLine);
   
   const $timestamp = $("<time>").addClass("timeago").attr("datetime", new Date(tweet.created_at));
   $tweet.append($timestamp);
   
   const $icons = $("<div>").addClass("tweet-icons");
   const $flagIcon = $("<i>").addClass("fa-solid fa-flag fa-fade");
-  const $flagLink = $("<a>").attr("href", "#").append($flagIcon);
-  $icons.append($flagLink);
-  
   const $retweetIcon = $("<i>").addClass("fa-solid fa-retweet fa-spin");
-  const $retweetLink = $("<a>").attr("href", "#").append($retweetIcon);
-  $icons.append($retweetLink);
-  
   const $heartIcon = $("<i>").addClass("fa-solid fa-heart fa-beat");
-  const $heartLink = $("<a>").attr("href", "#").append($heartIcon);
-  $icons.append($heartLink);
+  
+  $icons.append(
+    $("<a>").attr("href", "#").append($flagIcon),
+    $("<a>").attr("href", "#").append($retweetIcon),
+    $("<a>").attr("href", "#").append($heartIcon)
+  );
 
   $tweet.append($icons);
-
   
   return $tweet;
+};
+
+
+
+// F U N C T I O N:   R E N D E R   T W E E T S
+
+const renderTweets = function (tweets) {
+  const $tweetContainer = $("#tweets-container").empty();
+  tweets.forEach(function(tweet) {
+    const tweetElement = createTweetElement(tweet);
+    $tweetContainer.append(tweetElement);
+  });
 };
 
 
@@ -99,49 +107,79 @@ const loadTweets = function() {
 };
 
 
-// F U N C T I O N:   R E N D E R   T W E E T S
+// E R R O R --> Message length
 
-const renderTweets = function (tweets) {
-  const $tweetContainer = $("#tweets-container").empty();
-  tweets.forEach(function (tweet) {
-    const tweetElement = createTweetElement(tweet);
-    $tweetContainer.append(tweetElement);
-  });
+const showError = function(message) {
+  const $errorAlert = $("error-alert");
+  $errorAlert.text(message).slideDown();
 };
 
+const hideError = function () {
+  const $errorAlert = $("error-alert");
+  $errorAlert.text(message).slideUp();
+};
+  
+//FORM SUBMISSION
 
-// E V E N T   H A N D L E R
+const submitTweet = function() {
+  const tweetContent = $("#tweet-text").val();
+  hideError();
+};
 
-$(document).ready(function() {
-  $("time.timeago").timeago();
-  const tweetForm = $("#tweetForm");
-  tweetForm.on("submit", function (event) {
-    event.preventDefault();
-    const tweetData = tweetForm.serialize();
-    console.log(tweetData);
-    
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:8080/tweets",
-      contentType: "application/x-www-form-urlencoded",
-      data: tweetData,
-      success: function(response) {
-        loadTweets();
-      },
+if (!tweetContent) {
+  showError("Alert: Message cannot be empty.");
+} else if (tweetContent.length > 140) {
+  showError("Alert: Message is way too long to post here!");
+} else {
+  const tweetData = $("#tweetForm").serialize();
+  
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:8080/tweets",
+    contentType: "application/x-www-form-urlencoded",
+    data: tweetData,
+    success: function(response) {
+      $("#tweet-text").val("");
+      hideError();
+      loadTweets();
+    },
       error: function(error) {
         console.error("error:", error);
-      }
+      },
     });
-  });
-// loadTweets();
+  }
+};
+
+const tweetForm = $("#tweetForm");
+tweetForm.on("submit", function (event) {
+  event.preventDefault();
+  submitTweet();
 });
 
 loadTweets();
+$("time.timeago").timeago();
 
+          const tweetData = tweetForm.serialize();
+          console.log(tweetData);
+      
+      
+          const tweetContent = $("#tweet-text").val();
+          if(!tweetContent) {
+            showError("Alert: Message cannot be empty.");
+          } else if (tweetContent.length > 140) {
+            showError("Alert: Message is way too long to post here!");
+          } else {
+      
+          }
+        }
+      });
+    });
+  }
+};
 
-
-
-
-$(document).ready(function () {
-  $("time.timeago").timeago();
+const tweetForm = $("#tweetForm");
+tweetForm.on("submit", function (event) {
+  event.preventDefault();
+  submitTweet();
 });
+
